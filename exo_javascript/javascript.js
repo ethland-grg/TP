81c61ec439462f5cf97caff5cffba053cc97ef65
ethland-grg <you@example.com>
2020-10-22 15:27:04 +0400

    22/10/22

--- exo_javascript/javascript.js
+++ exo_javascript/javascript.js
@@ -1,8 +1,87 @@
-/*let main = document.getElementById("main-content");
+/* exo1 */
+/*
+let main = document.getElementById("main-content");
 console.log(main);
 
 let important = document.getElementsByClassName("important");
-console.log(important);*/
+console.log(important);
 
 let article = document.getElementsByTagName("article");
 console.log(article);
+
+let elt = document.querySelector("article > ul.important > li ");
+console.log(elt) ; */
+
+/*895649887+446558888888888888888888888888888888888888888888888888888888888888*/
+
+/* exo2 */
+/*
+const newElt = document.createElement("div");
+let elt = document.getElementById("main");
+elt.appendChild(newElt);
+console.log (newElt);
+
+
+let element = document.getElementById("main");
+element.innerHTML = "<ul><li>element1</li><li>element1</li></ul>";
+
+let desc = document.getElementsByTagName("p")
+*/
+
+/* Exo3 */
+/*
+var enfant=document.getElementById("child");
+var parent=document.getElementById("parent");
+var enfantCount=document.getElementById("child-count");
+var parentCount=document.getElementById("parent-count");
+var enfantScore = 0;
+var parentScore = 0;
+
+parent.addEventListener("click", function() {         
+    parentCount.innerHTML = ++parentScore;  
+  
+  console.log(parentCount);
+});
+
+enfant.addEventListener("click", function() {         
+    enfantCount.innerHTML = ++enfantScore;  
+  
+  console.log(parentCount);
+});
+*/
+
+/* Exo4 */
+const Nom = document.querySelector('input');
+const resName = document.getElementById('res-name');
+
+Nom.addEventListener('change', updateValue);
+
+function updateValue(a) {
+  resName.textContent = a.target.value;
+}
+/*
+const Genre = document.getElementById('gender');
+const resGenre = document.getElementById('res-gender');
+
+Genre.addEventListener('change', updateValue);
+
+function updateValue(b) {
+  resGenre.textContent = b.target.value;
+}   */
+
+const Genre = document.getElementById('gender');
+const resGenre = document.getElementById('res-gender');
+
+Genre.addEventListener('change', function(event){
+         resGenre.textContent = event.target.value;
+                       });
+
+
+
+
+/* git pull recuperer
+git push envoyer
+git add
+git comit pour metre nom*/
+
+
